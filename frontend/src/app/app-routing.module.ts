57add8f8888485bf2a08b057c18414280587540a
alancalumby <alancalumby@gmail.com>
2023-10-13 11:11:12 -0400

    Added listaCompras component and RouterLink

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ListaComprasComponent } from './views/lista-compras/lista-compras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "listaCompras",
+    component: ListaComprasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
