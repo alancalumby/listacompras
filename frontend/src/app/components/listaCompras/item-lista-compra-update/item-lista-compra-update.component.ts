9c6b163435393931fdafcc392dae3763e965ac6d
Alan Calumby <alancalumby@gmail.com>
2023-10-18 10:13:00 -0400

    Improved UpdateItem

--- frontend/src/app/components/listaCompras/item-lista-compra-update/item-lista-compra-update.component.ts
+++ frontend/src/app/components/listaCompras/item-lista-compra-update/item-lista-compra-update.component.ts
@@ -1,10 +1,44 @@
+import { ItemListaCompra } from './../item-lista-compra-modelo.model';
 import { Component } from '@angular/core';
+import { ItemListaCompraService } from '../item-lista-compra-servico.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-lista-compra-update',
   templateUrl: './item-lista-compra-update.component.html',
-  styleUrls: ['./item-lista-compra-update.component.css']
+  styleUrls: ['./item-lista-compra-update.component.css'],
 })
 export class ItemListaCompraUpdateComponent {
+  item: ItemListaCompra = {
+    id: 0,
+    descricao: '',
+    comprado: 0,
+  };
 
+  constructor(
+    private itemListaCompraService: ItemListaCompraService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    //o ! no final do comando serve para nao trazer como retorno o valor NULL (para o caso do parametro nao existir)
+    const id: string = this.activatedRoute.snapshot.paramMap.get('id')!;
+    this.itemListaCompraService.readById(id).subscribe((item) => {
+      this.item = item;
+    });
+  }
+
+  updateItemListaCompra(): void {
+    this.itemListaCompraService.update(this.item).subscribe(() => {
+      this.itemListaCompraService.showMessage(
+        'Item atualizado na lista de compras'
+      );
+      this.router.navigate(['/listaCompras']);
+    });
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/listaCompras']);
+  }
 }
--- frontend/src/app/components/listaCompras/lista-compras-add/lista-compras-add.component.ts
+++ frontend/src/app/components/listaCompras/lista-compras-add/lista-compras-add.component.ts
@@ -6,28 +6,30 @@ import { ItemListaCompra } from '../item-lista-compra-modelo.model';
 @Component({
   selector: 'app-lista-compras-add',
   templateUrl: './lista-compras-add.component.html',
-  styleUrls: ['./lista-compras-add.component.css']
+  styleUrls: ['./lista-compras-add.component.css'],
 })
 export class ListaComprasAddComponent {
-
   item: ItemListaCompra = {
     id: 0,
     descricao: '',
-    comprado: 0
-  }
+    comprado: 0,
+  };
 
-  constructor(private itemListaCompraService: ItemListaCompraService,
-    private router: Router) { }
+  constructor(
+    private itemListaCompraService: ItemListaCompraService,
+    private router: Router
+  ) {}
 
   addItemListaCompra(): void {
     this.itemListaCompraService.add(this.item).subscribe(() => {
-      this.itemListaCompraService.showMessage("Item adicionado na lista de compras");
-      this.router.navigate(["/listaCompras"]);
-    })
+      this.itemListaCompraService.showMessage(
+        'Item adicionado na lista de compras'
+      );
+      this.router.navigate(['/listaCompras']);
+    });
   }
 
   cancelar(): void {
-    this.router.navigate(["/listaCompras"])
+    this.router.navigate(['/listaCompras']);
   }
-
 }
