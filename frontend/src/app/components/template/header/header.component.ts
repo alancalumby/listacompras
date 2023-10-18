af29cafaf496220f867c85feef21883f7d64264e
Alan Calumby <alancalumby@gmail.com>
2023-10-18 14:11:49 -0400

    Atualizacao de titulo

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private headerService: HeaderService) {}
 
+  get title(): string {
+    return this.headerService.headerData.title;
+  }
+
+  get icone(): string {
+    return this.headerService.headerData.icone;
+  }
+
+  get routerUrl(): string {
+    return this.headerService.headerData.routeUrl;
+  }
 }
--- frontend/src/app/views/lista-compras/lista-compras.component.ts
+++ frontend/src/app/views/lista-compras/lista-compras.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-lista-compras',
   templateUrl: './lista-compras.component.html',
-  styleUrls: ['./lista-compras.component.css']
+  styleUrls: ['./lista-compras.component.css'],
 })
 export class ListaComprasComponent {
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de items',
+      icone: 'storefront',
+      routeUrl: '/listaCompras',
+    };
+  }
 
-  constructor (private router: Router) {}
-  
-  navigateToListaComprasAdd(): void{
-    this.router.navigate(['/listaCompras/add'])
+  navigateToListaComprasAdd(): void {
+    this.router.navigate(['/listaCompras/add']);
   }
 }
